147fd8f780ee94b2adad046bc1d0db162839b213
Yannis Menexes <johnmenex@gmail.com>
2018-02-21 14:57:12 +0200

    RealTimeGraphs.cs: Setting up for charts
    -New Constructor to receive incoming data
    -Public function ReceiveData behaving as a DataTransferer between MainWindow and RealTimeGraphs

--- MCT/RealTimeGraphs.Designer.cs
+++ MCT/RealTimeGraphs.Designer.cs
@@ -23,13 +23,31 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(813, 439);
+            this.zedGraphControl1.TabIndex = 0;
             // 
             // RealTimeGraphs
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(467, 310);
+            this.ClientSize = new System.Drawing.Size(838, 596);
+            this.Controls.Add(this.zedGraphControl1);
             this.Name = "RealTimeGraphs";
             this.Text = "RealTimeGraphs";
             this.ResumeLayout(false);
@@ -37,5 +55,8 @@
         }
 
         #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- MCT/RealTimeGraphs.cs
+++ MCT/RealTimeGraphs.cs
@@ -14,5 +14,24 @@ namespace MCT {
         public RealTimeGraphs() {
             InitializeComponent();
         }
+
+
+        public RealTimeGraphs(int _n_sensors, double[] _current_Values, int _sampling_rate) {
+            if (_n_sensors == 0)
+                return;
+
+            InitializeComponent();
+        }
+
+        public void ReceiveData(double[] _sensor_values) {
+            SensorValues = _sensor_values;
+        }
+
+        private protected GraphPane z;
+        private protected List<CurveItem> _curves;
+        double[] _sensorValues;
+
+        private protected List<CurveItem> Curves { get => _curves; set => _curves = value; }
+        private protected double[] SensorValues { get => _sensorValues; set => _sensorValues = value; }
     }
 }
