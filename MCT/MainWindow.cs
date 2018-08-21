eab7eb9bf7f43450aa14b0575848de767ba60c38
YiannisMenex <johnmenex@gmail.com>
2018-08-21 16:20:26 +0300

    Project: Clearing

--- MCT/MainWindow.cs
+++ MCT/MainWindow.cs
@@ -25,14 +25,9 @@ THE SOFTWARE.
 #define demo
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Management;
 using System.IO.Ports;
 using System.Diagnostics;
 using System.IO;
